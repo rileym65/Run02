5a4b2f589ed943385902643eaff2cf83204ba028
Mike Riley <mikehriley@hotmail.com>
2021-11-05 06:17:20 -0700

    Added INP and OUT support, reading/writing Linux file numbers Added documentation for conditional breakpoints

--- cpu.c
+++ cpu.c
@@ -1542,8 +1542,29 @@ void cpuCycle(CPU *cpu) {
                   strcat(tbuffer, tline);
                   }
                 break;
+           case 0x01:                                                      // OUT
+           case 0x02:
+           case 0x03:
+           case 0x04:
+           case 0x05:
+           case 0x06:
+           case 0x07:
+                i = readMem(cpu, cpu->r[cpu->x]++);
+                write((cpu->n & 0xf)-1, &i, 1);
+                break;
            case 0x08:                                                      // 1805 inst
                 if (use1805) cpu1805(cpu);
+                break;
+           case 0x09:                                                      // OUT
+           case 0x0a:
+           case 0x0b:
+           case 0x0c:
+           case 0x0d:
+           case 0x0e:
+           case 0x0f:
+                read((cpu->n & 0xf)-9, &i, 1);
+                writeMem(cpu, cpu->r[cpu->x], i);
+                cpu->d = i;
                 break;
            }
          break;
